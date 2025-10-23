f1746bc5ea0a9fa86b59f76bc4b74bfc9ac948df
Miteshkothe <139582340+Miteshkothe@users.noreply.github.com>
2025-10-23 23:25:59 +0530

    Delete myHandler.java

--- myHandler.java
+++
@@ -1,8 +0,0 @@
-package com.restApi.journalApp.Config;
-
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-public class myHandler extends TextWebSocketHandler {
-
-}
